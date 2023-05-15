c9fbe225f167d9d47c8937ea97f3d4e9ac9b21fb
gorkemyar <gorkemyar2000@hotmail.com>
2023-05-15 17:39:35 -0400

    swipe angle

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -7,10 +7,14 @@ public class Board : MonoBehaviour
     public int width;
     public int height;
     public GameObject tilePrefab;
-    private BackgroundTile[,] allTiles;
+    public GameObject[] dots;
+
+    public GameObject[,] allDots;
+    private BackgroundTile[,] allTiles;    
 
     void Start(){
         allTiles = new BackgroundTile[width, height];
+        allDots = new GameObject[width, height];
         SetUp();
     }
 
@@ -21,6 +25,13 @@ public class Board : MonoBehaviour
                 GameObject backgroundTile = Instantiate(tilePrefab, tempPosition, Quaternion.identity) as GameObject;
                 backgroundTile.transform.parent = this.transform;
                 backgroundTile.name = "( " + i + ", " + j + " )";
+
+                int dotToUse = Random.Range(0, dots.Length);
+                GameObject dot = Instantiate(dots[dotToUse], tempPosition, Quaternion.identity);
+                dot.transform.parent = this.transform;
+                dot.name = "( " + i + ", " + j + " )";
+
+                allDots[i, j] = dot;
             }
         }
     }
